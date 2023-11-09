fd4529fd8ff59765bbdbe24255dce6375d08ab44
supergjason <supergjason@gmail.com>
2023-11-08 16:56:56 -0800

    added resistance temperature dependence

--- FuseThermodynamics.m
+++ FuseThermodynamics.m
@@ -1,22 +1,36 @@
-function dTdt = FuseThermodynamics(t, state, cvol, Lfuse, d, Lblock, Wblock, Hblock, I, rho, rcond, e)
-    Tfuse = state(1)
-    Tblock = state(2)
-    Tfuse
+function dTdt = FuseThermodynamics(t, state, cvol, Lfuse, d, Lblock, Wblock, Hblock, I, rho, rcond, e, a)
+    TfuseCondRad = state(1);
+    Tblock = state(2);
+    TfuseCond = state(3);
+    TfuseRad = state(4);
+    TfuseNone = state(5);
 
-    R = (rho * Lfuse) / (pi * d^2 / 4); %Fuse resistance
+    R0 = (rho * Lfuse) / (pi * d^2 / 4); %Fuse resistance
+    R = R0 * (1+a*(TfuseCondRad-20));
+    R1 = R0 * (1+a*(TfuseCond-20));
+    R2 = R0 * (1+a*(TfuseRad-20));
+    R3 = R0 * (1+a*(TfuseNone-20));
 
     Qjoule = (I^2 * R); %Joule heating from electrical current
-    Qcond = -(1/rcond) * (Tfuse - Tblock); %Conduction from fuse to block
-    Qrad = -(5.67 * 10^(-8)) * e * (pi * d * Lfuse) * Tfuse^4; %Radiation away from fuse
+    Qcond = -(1/rcond) * (TfuseCondRad - Tblock); %Conduction from fuse to block
+    Qrad = -(5.67 * 10^(-8)) * e * (pi * d * Lfuse) * (TfuseCondRad+273)^4; %Radiation away from fuse
+
+    Qjoule1 = (I^2 * R1);
+    Qcond1 = -(1/rcond) * (TfuseCond - Tblock); %Conduction from fuse to block
+    
+    Qjoule2 = (I^2 * R2);
+    Qrad2 = -(5.67 * 10^(-8)) * e * (pi * d * Lfuse) * (TfuseRad+273)^4; %Radiation away from fuse
+
+    Qjoule3 = (I^2 * R3);
 
     Cvfuse = (cvol * Lfuse * pi * (d^2 / 4)); %Heat capacity of fuse
     Cvblock = 2 * (cvol * Lblock * Wblock * Hblock); %Heat capacity of block
 
-    Tfuse_dot = (Qjoule + Qcond + Qrad)/Cvfuse %Heat transfer of various scenarios (explained in main script)
+    Tfuse_dot = (Qjoule + Qcond + Qrad)/Cvfuse; %Heat transfer of various scenarios (explained in main script)
     Tblock_dot = -(Qjoule + Qcond)/Cvblock;
-    Tfuse_dotcond = (Qjoule + Qcond)/Cvfuse;
-    Tfuse_dotrad = (Qjoule + Qrad)/Cvfuse;
-    Tfuse_dotnone = (Qjoule/Cvfuse)
+    Tfuse_dotcond = (Qjoule1 + Qcond1)/Cvfuse;
+    Tfuse_dotrad = (Qjoule2 + Qrad2)/Cvfuse;
+    Tfuse_dotnone = (Qjoule3/Cvfuse);
 
     dTdt = [Tfuse_dot; Tblock_dot; Tfuse_dotcond; Tfuse_dotrad; Tfuse_dotnone];
 end
--- FuseThermodynamics_main.m
+++ FuseThermodynamics_main.m
@@ -6,40 +6,31 @@ d = 0.001; %diameter of fuse (m)
 Lblock = 0.05; %length of one terminal block (m)
 Wblock = 0.04; %width of terminal block (m)
 Hblock = 0.02; %height of terminal block (m)
-I = 200; %current (A)
+I = 160; %current (A)
 rho = 2.63 * 10^(-8); %aluminum resistivity (ohm m^2/m)
-rcond = 300; %thermal resistance between fuse and block (K/W)
+rcond = 100; %thermal resistance between fuse and block (K/W)
 e = 0.5; %fuse emissivity
-
+a = 0.00429; %aluminum resistance temperature coefficient
 %Time range
 T = 5; %max time
 times = [0 T];
 
 %Initial conditions: [Tfuse, Tblock, Tfusecond, Tfuserad, Tfusenone]
-initial_conditions = [25.0, 21.0, 25.0, 25.0, 25.0];
+initial_conditions = [25.0, 25.0, 25.0, 25.0, 25.0];
 
 %Numerically solve ODE and plot solution
-[t, solution] = ode45(@(t, state) FuseThermodynamics(t, state, cvol, Lfuse, d, Lblock, Wblock, Hblock, I, rho, rcond, e), times, initial_conditions);
+[t, solution] = ode45(@(t, state) FuseThermodynamics(t, state, cvol, Lfuse, d, Lblock, Wblock, Hblock, I, rho, rcond, e, a), times, initial_conditions);
 
 % Plot temperature profiles of scenarios: 1. Fuse Temp with Joule heating, conduction, &
 % radiation; 2. Block Temp; 3. Fuse Temp with joule & conduction; 4. Fuse
-% temp with joule & radiation; 5. Fuse temp with only joule (some lines may
-% be covered by others due to radiation being negligible at low
-% temperatures)
-subplot(2, 1, 1);
-plot(t, solution(:, 1), 'b', t, solution(:, 2), 'r', t, solution(:, 3), 'm'); %, t, solution(:, 4), 'g') , t, solution(:, 5), 'k');
+% temp with joule & radiation; 5. Fuse temp with only joule
+
+yline(660, '-.', "melting point"); %aluminum melting point
+hold on;
+plot(t, solution(:, 1), 'b', t, solution(:, 2), 'r', t, solution(:, 3), 'm', t, solution(:, 4), 'g' , t, solution(:, 5), 'k');
+ylim([0 3000])
+hold off;
 xlabel('Time (s)');
 ylabel('Temperature (°C)');
-title('Temperature Profiles');
-legend('Tfuse', 'Tblock', 'Tfusecond');% 'Tfuserad', 'Tfusenone');
-
-%{ 
-%Calculate and plot derivatives
-dTdt = diff(solution) ./ diff(t);
-subplot(2, 1, 2);
-plot(t(2:end), dTdt(:, 1), 'b', t(2:end), dTdt(:, 2), 'r');
-xlabel('Time (s)');
-ylabel('dT/dt (°C/s)');
-title('Derivatives of Temperature Profiles');
-legend('dTfuse/dt', 'dTblock/dt');
-%}
+title("Temperature Profiles at " + I + "A");
+legend('','TfuseCondRad', 'Tblock', 'TfuseCond', 'TfuseRad', 'TfuseNone');
